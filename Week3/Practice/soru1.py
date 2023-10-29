7617250656fcd11f7e6bc03405c309708200f7bb
Rojen Arda Şeşen <73838947+rojenarda@users.noreply.github.com>
2023-10-29 11:16:35 +0300

    Delete Week3 directory

--- Week3/Practice/soru1.py
+++
@@ -1,8 +0,0 @@
-s = input("Kelime gir:")
-new_s = ""
-for letter in s:
-    if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u" or letter == "A" or letter == "E" or letter == "I" or letter == "O" or letter == "U":
-        continue
-    else:
-        new_s = new_s + letter
-print(new_s)
